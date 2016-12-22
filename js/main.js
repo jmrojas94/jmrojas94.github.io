9d1343c533c09f0a2db3058293b8028f8c3669ff
Juan Rojas <jmr5150@gmail.com>
2016-12-22 02:03:37 -0600

    Adds nav, pages, and some style.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
+})
