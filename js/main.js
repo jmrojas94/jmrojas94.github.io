fe43584a9933390791f31b0a50b0d42c090e4d84
Juan Rojas <jmr5150@gmail.com>
2016-12-30 16:14:50 -0600

    Adds about me content, adds typed header.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
 	$('#menuToggle, .menu-close').on('click', function(){
 		$('#menuToggle').toggleClass('active');
 		$('body').toggleClass('body-push-toleft');
-		$('#theMenu').toggleClass('menu-open');
+		$('#menu').toggleClass('menu-open');
+	});
+
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	  	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	    	var target = $(this.hash);
+	    	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	    if (target.length) {
+	      	$('html, body').animate({
+	        	scrollTop: target.offset().top
+	      	}, 1000);
+	      		return false;
+	    	}
+	  	}
 	});
 })
