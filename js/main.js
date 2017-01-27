3a2e60f834aa41582d0652ba266a28921e4d0c58
Juan Rojas <jmr5150@gmail.com>
2017-01-27 17:52:36 -0600

    minified js files and added modals to projects.

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 		$('#menu').toggleClass('menu-open');
 	});
 
-	$('a[href*="#"]:not([href="#"])').click(function() {
+	$('a[href*="#"]:not([href="#"],[href*="#animatedModal"])').click(function() {
 		$('#menu').toggleClass('menu-open');
 	  	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 	    	var target = $(this.hash);
@@ -62,5 +62,7 @@ $(document).ready(function(){
 	    }, 500);
 		
 	});
+
+
 })
 
