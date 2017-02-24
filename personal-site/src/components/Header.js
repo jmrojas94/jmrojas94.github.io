f6392533e34b362e731d7d169c2b32b1894d175f
Juan Rojas <jmr5150@gmail.com>
2017-02-23 19:53:54 -0600

    SAVE POINT.

--- personal-site/src/components/Header.js
+++ personal-site/src/components/Header.js
@@ -6,8 +6,8 @@ export default class Header extends React.Component {
     	<div id="home" className="App-header">
     		<div className="container-fluid text-center">
     			<div className="intro col-xs-12">
-    				<h1>[Juan Rojas]</h1>
-    				<h1><span id="head-title">[I Am </span><span className="typed-cursor">_</span>]</h1>
+    				<h1>[ Juan Rojas ]</h1>
+    				<h2><span id="head-title">[ I Am </span><span className="typed-cursor">_</span> ]</h2>
     			</div>
     		</div>
       </div>
