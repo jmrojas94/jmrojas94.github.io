7d618dc803d9c75d2f8fd1bbd8b2b1b59130614e
Juan Rojas <jmr5150@gmail.com>
2017-04-07 20:12:34 -0500

    FSDLk

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,20 +10,20 @@ export default class Footer extends React.Component {
           <div className="row">
         		<footer className="text-center col-xs-12">
               <h2>Juan Rojas</h2>
-              <p><b>Email:</b> juanmrojas94@gmail.com</p>
+              <p className="email"><b>Email:</b> juanmrojas94@gmail.com</p>
               <div className="social">
-                <a href="#"><FontAwesome name="facebook"
+                <a href="https://www.facebook.com/juan.rojas.1000?ref=bookmarks"><FontAwesome name="facebook" size='2x'/>
+                </a>
+                <a href="https://twitter.com/jmrojas94"><FontAwesome name="twitter"
+                          size='2x'/>
+                </a>
+                <a href="https://www.linkedin.com/in/jmrojas94/"><FontAwesome name="linkedin"
                           
                           size='2x'/></a>
-                <a href="#"><FontAwesome name="twitter"
-                          size='2x'/></a>
-                <a href="#"><FontAwesome name="linkedin"
+                <a href="https://github.com/jmrojas94"><FontAwesome name="github"
                           
                           size='2x'/></a>
-                <a href="#"><FontAwesome name="github"
-                          
-                          size='2x'/></a>
-                <a href="#"><FontAwesome name="instagram"
+                <a href="https://www.instagram.com/jrojastx/"><FontAwesome name="instagram"
                           
                           size='2x'/></a>
                 </div>
